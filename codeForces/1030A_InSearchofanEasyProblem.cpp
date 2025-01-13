4743c545716cedd1bfbaa507f538e15fe001bcef
ayushgoyal04 <ayushgoyal434@gmail.com>
2025-01-14 00:48:32 +0530

    1030

---
+++ codeForces/1030A_InSearchofanEasyProblem.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a;
+    for(int i=0; i<n; i++){
+        cin>>a;
+        if(a==1){
+            cout<<"HARD";
+            return 0;
+        }
+    }
+    cout<<"EASY";
+}
