6c9fd20ce6e10fb24d1444aa70c01b5bb037611c
AYUSH GOYAL <112977429+ayushgoyal04@users.noreply.github.com>
2025-07-27 16:25:58 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.55%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if (nums.size()<1) return {-1, -1};
+        if (nums.size()==1 && nums[0] == target) return {0, 0};
+        if (nums.size()==1 && nums[0] != target) return {-1, -1};
+
+        int left = 0, right = nums.size()-1, mid;
+        int leftans = -1, rightans = -1;
+        
+        // finding the left ans
+        while(left <= right) {
+            mid = right + (left - right) /2;
+
+            if(nums[mid] < target) {
+                left = mid + 1;
+            }
+            else if(nums[mid]>target) {
+                // leftans = mid;
+                right = mid - 1;
+            }
+            if(nums[mid] == target) {
+                leftans = mid;
+                right = mid - 1;
+            }
+        }
+
+        // finding the right ans
+        left = 0; 
+        right = nums.size()-1;
+        while(left <= right) {
+            mid = right + (left - right) /2;
+            
+            if(nums[mid] < target) {
+                left = mid + 1;
+            }
+            else if(nums[mid]>target) {
+                // leftans = mid;
+                right = mid - 1;
+            }
+            if(nums[mid] == target) {
+                rightans = mid;
+                left = mid + 1;
+            }
+        }
+
+        return {leftans, rightans};
+    }
+};
