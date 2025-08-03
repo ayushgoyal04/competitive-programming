8173da8f51b639a5e4b49dd93b15030e32612737
AYUSH GOYAL <112977429+ayushgoyal04@users.noreply.github.com>
2025-08-03 22:47:07 +0530

    Time: 15 ms (34.07%), Space: 35.1 MB (36.15%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -5,40 +5,21 @@ public:
         
         // [1,2,3,4,5,6,7,8,9,10] 10 5
         int dayssum = 0;
-        int day = 1;
         for(int i = 0; i < weights.size(); i++){
             if(dayssum+weights[i] <= capacity){
                 dayssum += weights[i]; // 5
             }
             else{
                 days--; // 4 3
-                dayssum = 0;
-                i--;
                 if(days<=0){
                     return false;
                 }
+                i--;
+                dayssum = 0;
             }
         }
         return true;
     }
-
-//     bool test(vector<int>& weights, int capacity, int days) {
-//     int dayCount = 1;     // Start with first day
-//     int currentLoad = 0;  // Track total load of current day
-
-//     for (int w : weights) {
-//         if (currentLoad + w <= capacity) {
-//             currentLoad += w;
-//         } else {
-//             dayCount++;        // New day
-//             currentLoad = w;   // Start with current weight
-//             if (dayCount > days) return false;
-//         }
-//     }
-
-//     return true;
-// }
-
 
     int shipWithinDays(vector<int>& weights, int days) {
         int left = *max_element(weights.begin(), weights.end());
