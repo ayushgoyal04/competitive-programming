656e9afb9f263abea7ad093047cf386d7781f2ee
AYUSH GOYAL <112977429+ayushgoyal04@users.noreply.github.com>
2025-08-04 18:32:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (42.51%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) return 1;
-        if (n < 0) return (double)1.0/x * myPow((double)1.0/x, -(++n));
-        if (n % 2 == 1) return x * myPow(x*x, n/2);
-        else return myPow(x*x, n/2);
+        // O(N) approach= BRUTE
+        // if n is positive we can just do a for loop with ans = ans * ans (resiprocal for negative)
+        // x^-n = 1/x^n 
+        // edge case- the size of integer
+
+        // optimal - 2^10 = (2*2)^5 = 4^5- this decrease our operations
+        // 4^5 = 16^2
+        // 16^2 = 256
+
+        // time com = log2(n)
+        double ans = 1.0;
+        long long nn = n;
+        if(nn<0) nn = -1 * nn;
+        while(nn){
+            if(nn%2) {
+                ans = ans * x;
+                nn = nn -1;
+            }
+            else{
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+        if(n<0) ans = (double)(1.0) / (double)(ans);
+        return ans;
     }
 };
