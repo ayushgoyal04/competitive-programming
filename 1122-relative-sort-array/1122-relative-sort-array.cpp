81da4abfcdcbbdb5eab8151ee8ba61bae9b95993
AYUSH GOYAL <112977429+ayushgoyal04@users.noreply.github.com>
2026-05-05 20:10:16 +0530

    Time: 0 ms (100%), Space: 11 MB (84.75%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,36 +1,68 @@
+// class Solution {
+// public:
+//     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+//         map<int, int> mp;
+//         int n = arr1.size();
+//         int m = arr2.size();
+//         for( int i = 0; i < n; i++) {
+//             mp[arr1[i]]++;
+//             for(int j = 0; j < m; j++)
+//             {
+//                 if(arr1[i]==arr2[j])
+//                 arr1[i] = -1;
+//             }
+//         }
+//         vector<int> ans;
+//         for( auto it : arr2){
+//             int len = mp[it];
+//             int val = it;
+//             for(int i = 0; i < len; i++)
+//             {
+//                 ans.push_back(val);
+//             }
+//         }
+//         vector<int> temp;
+//         for(auto num : arr1){
+//             if(num != -1){
+//                 temp.push_back(num);
+//             }
+//         }
+//         sort(temp.begin(), temp.end());
+//         for(auto i : temp){
+//             ans.push_back(i);
+//         }               
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         map<int, int> mp;
-        int n = arr1.size();
-        int m = arr2.size();
-        for( int i = 0; i < n; i++) {
-            mp[arr1[i]]++;
-            for(int j = 0; j < m; j++)
-            {
-                if(arr1[i]==arr2[j])
-                arr1[i] = -1;
-            }
+
+        // Count frequency
+        for (int num : arr1) {
+            mp[num]++;
         }
+
         vector<int> ans;
-        for( auto it : arr2){
-            int len = mp[it];
-            int val = it;
-            for(int i = 0; i < len; i++)
-            {
-                ans.push_back(val);
+
+        // Add elements in arr2 order
+        for (int x : arr2) {
+            while (mp[x] > 0) {
+                ans.push_back(x);
+                mp[x]--;
             }
         }
-        vector<int> temp;
-        for(auto num : arr1){
-            if(num != -1){
-                temp.push_back(num);
+
+        // Add remaining elements sorted
+        for (auto &p : mp) {
+            while (p.second > 0) {
+                ans.push_back(p.first);
+                p.second--;
             }
         }
-        sort(temp.begin(), temp.end());
-        for(auto i : temp){
-            ans.push_back(i);
-        }               
+
         return ans;
     }
 };
